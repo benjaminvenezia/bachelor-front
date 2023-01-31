5b597932414e3a3dd217713d65e7814330f30347
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 20:03:54 +0100

    add register page

--- navigation/AuthNavigator.tsx
+++ navigation/AuthNavigator.tsx
@@ -2,14 +2,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import ROUTES from "../constants/routes";
 import About from "../screens/About";
+import Register from "../screens/Register";
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName={ROUTES.HOME}>
       <Stack.Screen name={ROUTES.HOME} component={Home} />
-      <Stack.Screen name={ROUTES.ABOUT} component={About} />
+      <Stack.Screen name={ROUTES.REGISTER} component={Home} />
+      <Stack.Screen name={ROUTES.ABOUT} component={Register} />
     </Stack.Navigator>
   );
 };

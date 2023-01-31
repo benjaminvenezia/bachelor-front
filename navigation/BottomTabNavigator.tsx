91b6fa4242360e73cee3ea3229d1542580b6647d
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 21:19:56 +0100

    update tabs labels

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -7,12 +7,12 @@ const Tab = createBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name={ROUTES.HOME_TAB} component={Home} />
-      <Tab.Screen name={ROUTES.HABITS_TAB} component={Habits} />
-      <Tab.Screen name={ROUTES.GAGE_TAB} component={Gage} />
-      <Tab.Screen name={ROUTES.TEAM_TAB} component={Team} />
-      <Tab.Screen name={ROUTES.SUGGEST_TAB} component={Suggestion} />
-      <Tab.Screen name={ROUTES.SHOP_TAB} component={Shop} />
+      <Tab.Screen options={{ tabBarLabel: "Menu" }} name={ROUTES.HOME_TAB} component={Home} />
+      <Tab.Screen options={{ tabBarLabel: "Habitudes" }} name={ROUTES.HABITS_TAB} component={Habits} />
+      <Tab.Screen options={{ tabBarLabel: "Gage" }} name={ROUTES.GAGE_TAB} component={Gage} />
+      <Tab.Screen options={{ tabBarLabel: "Vous" }} name={ROUTES.TEAM_TAB} component={Team} />
+      <Tab.Screen options={{ tabBarLabel: "Suggestions" }} name={ROUTES.SUGGEST_TAB} component={Suggestion} />
+      <Tab.Screen options={{ tabBarLabel: "Boutique" }} name={ROUTES.SHOP_TAB} component={Shop} />
     </Tab.Navigator>
   );
 };

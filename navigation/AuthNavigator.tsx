81013799346b56e99cf761c104988ba7879c1e72
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 20:53:20 +0100

    add bottom tab navigation.

--- navigation/AuthNavigator.tsx
+++ navigation/AuthNavigator.tsx
@@ -1,16 +1,17 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import ROUTES from "../constants/routes";
 import { About, Register, Login, Home } from "../screens";
+import BottomTabNavigator from "./BottomTabNavigator";
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => {
   return (
     <Stack.Navigator initialRouteName={ROUTES.REGISTER}>
-      <Stack.Screen name={ROUTES.HOME} component={Home} />
       <Stack.Screen name={ROUTES.REGISTER} component={Register} />
       <Stack.Screen name={ROUTES.LOGIN} component={Login} />
       <Stack.Screen name={ROUTES.ABOUT} component={About} />
+      <Stack.Screen name={ROUTES.HOME} component={BottomTabNavigator} />
     </Stack.Navigator>
   );
 };
--- navigation/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AuthNavigator } from "./AuthNavigator";
-export { default as BottomTabNavigator } from "./BottomTabNavigator";

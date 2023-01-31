b3d7baa7a2d73e4361e3c98fc67d6ffe0e72f366
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 20:32:52 +0100

    improve the routes adding tabs and drawer variations.

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,16 +1,50 @@
 export default {
   LOGIN: "Login",
   REGISTER: "Register",
+
   HOME: "Home",
+  HOME_TAB: "Home Tab",
+  HOME_DRAWER: "Home Drawer",
+
   CONNECTUS: "ConnectUs",
+  CONNECTUS_TAB: "ConnectUs Tab",
+  CONNECTUS_DRAWER: "ConnectUs Drawer",
+
   HABITS: "Habits",
+  HABITS_TAB: "Habits Tab",
+  HABITS_DRAWER: "Habits Drawer",
+
   PROGRAMTASKS: "ProgramTasks",
+  PROGRAMTASKS_TAB: "ProgramTasks Tab",
+  PROGRAMTASKS_DRAWER: "ProgramTasks Drawer",
+
   SHOP: "Shop",
+  SHOP_TAB: "Shop Tab",
+  SHOP_DRAWER: "Shop Drawer",
+
   THEME: "Theme",
+  THEME_TAB: "Theme Tab",
+  THEME_DRAWER: "Theme Drawer",
+
   TEAM: "Team",
+  TEAM_TAB: "Team Tab",
+  TEAM_DRAWER: "Team Drawer",
+
   USER: "User",
+  USER_TAB: "User Tab",
+  USER_DRAWER: "User Drawer",
+
   GAGE: "Gage",
+  GAGE_TAB: "Gage Tab",
+  GAGE_DRAWER: "Gage Drawer",
+
   ABOUT: "About",
-  WELCOME: "Welcome",
+  ABOUT_TAB: "About Tab",
+  ABOUT_DRAWER: "About Drawer",
+
   SUGGEST: "Suggestion",
+  SUGGEST_TAB: "Suggestion Tab",
+  SUGGEST_DRAWER: "Suggestion Drawer",
+
+  WELCOME: "Welcome",
 };

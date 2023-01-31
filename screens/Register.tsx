c71a1a8672d5ba6da66776719c1507f733719107
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 20:11:12 +0100

    add basic navigation between register and login

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
-const Register = () => {
+import { View, Text, Button, StyleSheet } from "react-native";
+import ROUTES from "../constants/routes";
+const Register = ({ navigation }) => {
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.wrapper}>
+      <Text>Inscription</Text>
+
+      <Text>Déjà un compte?</Text>
+      <Button title="connexion" onPress={() => navigation.navigate(ROUTES.LOGIN)} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+});
+
 export default Register;

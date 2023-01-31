68d7ba60036b4d98ead3db62c6375ebcb994795a
Benjamin <Benjaminvenezia@hotmail.com>
2023-01-31 20:06:41 +0100

    remove duplicated components

--- screens/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-const SignIn = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  );
-};
-export default SignIn;
--- screens/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-const SignUp = () => {
-  return (
-    <View>
-      <Text>Inscription</Text>
-    </View>
-  );
-};
-export default SignUp;

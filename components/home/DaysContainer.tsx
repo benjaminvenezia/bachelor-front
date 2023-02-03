1857f48a030083ad2768ee24e3f9afaa5aca0617
Benjamin <Benjaminvenezia@hotmail.com>
2023-02-03 09:43:14 +0100

    add constants to manage days.

--- components/home/DaysContainer.tsx
+++ components/home/DaysContainer.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import Day from "./Day";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import DAYS from "../../constants/days";
 
 const DaysContainer = () => {
   const day = useSelector((state: RootState) => state.day);
@@ -9,13 +10,13 @@ const DaysContainer = () => {
   return (
     <View style={styles.container}>
       <Text>The day active is : {day["activeDay"]}</Text>
-      <Day>Lun</Day>
-      <Day>Mar</Day>
-      <Day>Mer</Day>
-      <Day>Jeu</Day>
-      <Day>Ven</Day>
-      <Day>Sam</Day>
-      <Day>Dim</Day>
+      <Day>{DAYS.MONDAY}</Day>
+      <Day>{DAYS.TUESDAY}</Day>
+      <Day>{DAYS.WEDNESDAY}</Day>
+      <Day>{DAYS.THURSDAY}</Day>
+      <Day>{DAYS.FRIDAY}</Day>
+      <Day>{DAYS.SATURDAY}</Day>
+      <Day>{DAYS.SUNDAY}</Day>
     </View>
   );
 };
---
+++ constants/days.ts
@@ -0,0 +1,30 @@
+export default {
+  /**
+   * Lun
+   */
+  MONDAY: "Lun",
+  /**
+   * Mar
+   */
+  TUESDAY: "Mar",
+  /**
+   * Mer
+   */
+  WEDNESDAY: "Mer",
+  /**
+   * Jeu
+   */
+  THURSDAY: "Jeu",
+  /**
+   * Ven
+   */
+  FRIDAY: "Ven",
+  /**
+   * Sam
+   */
+  SATURDAY: "Sam",
+  /**
+   * Dim
+   */
+  SUNDAY: "Dim",
+};

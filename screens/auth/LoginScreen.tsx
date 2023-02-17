8adaedbc13adc62fb52d3d8a86ec9ff010e62c51
Benjamin <Benjaminvenezia@hotmail.com>
2023-02-17 18:37:52 +0100

    extract the login axios in screen temporarily and doing it work.

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -3,20 +3,43 @@ import ROUTES from "../../constants/routes";
 import React from "react";
 import { Button, Title, Input } from "../../components";
 import { GlobalStyles } from "../../constants/style";
+import { useState } from "react";
+import axios from "axios";
 
 const LoginScreen = ({ navigation }: any) => {
-  const [mail, onChangeMail] = React.useState("");
-  const [password, onChangePassword] = React.useState("");
+  const [email, onChangeMail] = useState("ben@gmail.com");
+  const [password, onChangePassword] = useState("password");
+  const [loading, setLoading] = useState(false);
+  const [fromFetch, setFromFetch] = useState(false);
+  const [axiosData, setAxiosData] = useState(null);
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    setFromFetch(false);
+    setLoading(true);
+
+    axios.post("http://127.0.0.1:8000/api/login/", { email: email, password: password }).then((response) => {
+      console.log("getting data from axios", response.data);
+      setLoading(false);
+      setAxiosData(response.data);
+    });
+
+    navigation.navigate(ROUTES.HOME);
+  };
+
+  {
+    console.log("pututut ", axiosData);
+  }
 
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <Title style={styles.title}>Connexion</Title>
         <View style={styles.inputsContainer}>
-          <Input onChangeHandler={onChangeMail} value={mail} placeholder="Mail" keyboard="email-address" />
+          <Input onChangeHandler={onChangeMail} value={email} placeholder="Mail" keyboard="email-address" />
           <Input onChangeHandler={onChangePassword} value={password} placeholder="Mot de passe" keyboard="default" />
 
-          <Button size={GlobalStyles.buttons.lg} onPress={() => navigation.navigate(ROUTES.HOME)}>
+          <Button size={GlobalStyles.buttons.lg} onPress={handleClick}>
             Valider
           </Button>
         </View>
--- utils/http.ts
+++ utils/http.ts
@@ -3,10 +3,29 @@ import axios from "axios";
 const path = "localhost:8000/api/";
 
 type User = {
-  mail: string;
+  email: string;
   password: string;
 };
 
 export const login = (user: User) => {
-  axios.post(path + "login", user);
+  const headers = {
+    Accept: "application/vnd.api+json",
+    "Content-Type": "application/vnd.api+json",
+    // Authorization: apiKey,
+  };
+
+  const { email, password } = user;
+
+  try {
+    const p = axios.post(
+      "127.0.0.1:8000/api/login",
+      {
+        email: email,
+        password: password,
+      },
+      { headers }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };

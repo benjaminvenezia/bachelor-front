148a2ff1f388948e292739a3d80798f34ef36844
Benjamin <Benjaminvenezia@hotmail.com>
2023-06-07 16:37:45 +0200

    Add icons on the bottom menu. :)

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,18 +3,73 @@ import ROUTES from "../constants/routes";
 import { HomeScreen, HabitsScreen, TeamScreen, SuggestionScreen, ShopScreen } from "../screens";
 import GageStackNavigator from "./GageStacknavigator";
 import TeamStackNavigator from "./TeamStackNavigator";
+import { Feather, MaterialCommunityIcons, FontAwesome5, Entypo } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen options={{ tabBarLabel: "Menu" }} name={ROUTES.HOME_TAB} component={HomeScreen} />
-      <Tab.Screen options={{ tabBarLabel: "Habitudes" }} name={ROUTES.HABITS_TAB} component={HabitsScreen} />
-      <Tab.Screen options={{ tabBarLabel: "Gage" }} name={ROUTES.GAGE_TAB} component={GageStackNavigator} />
-      <Tab.Screen options={{ tabBarLabel: "Vous" }} name={ROUTES.TEAM_TAB} component={TeamStackNavigator} />
-      <Tab.Screen options={{ tabBarLabel: "Suggestions" }} name={ROUTES.SUGGEST_TAB} component={SuggestionScreen} />
-      <Tab.Screen options={{ tabBarLabel: "Boutique" }} name={ROUTES.SHOP_TAB} component={ShopScreen} />
+      <Tab.Screen
+        name={ROUTES.HOME_TAB}
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Menu",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="home" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name={ROUTES.HABITS_TAB}
+        component={HabitsScreen}
+        options={{
+          tabBarLabel: "Habitudes",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome5 name="socks" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name={ROUTES.GAGE_TAB}
+        component={GageStackNavigator}
+        options={{
+          tabBarLabel: "Gage",
+          tabBarIcon: ({ color, size }) => {
+            return <MaterialCommunityIcons name="sword" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name={ROUTES.TEAM_TAB}
+        component={TeamStackNavigator}
+        options={{
+          tabBarLabel: "Vous",
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="users" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name={ROUTES.SUGGEST_TAB}
+        component={SuggestionScreen}
+        options={{
+          tabBarLabel: "Suggestions",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="list" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Boutique",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="shopping-cart" size={size} color={color} />;
+          },
+        }}
+        name={ROUTES.SHOP_TAB}
+        component={ShopScreen}
+      />
     </Tab.Navigator>
   );
 };
